9f187e9cda69f9198c2b907f328ac0408693d82b
Cahyo <cahyo.p@gmail.com>
2016-03-05 08:01:32 +0800

    created component type library

--- SchemChecker/SchemComponent.py
+++ SchemChecker/SchemComponent.py
@@ -1,7 +1,16 @@
 
-class SchematicComponent(object):
+class SchematicSymbol(object):
 
     def __init__(self):
         self.id = ''
         self.type = ''
         self.pins = {}
+        self.state = []
+        self.links = {}
+
+
+class SchematicComponent(object):
+
+    def __init__(self):
+        self.type = ''
+        self.links = {}
--- SchemChecker/checker.py
+++ SchemChecker/checker.py
@@ -2,14 +2,40 @@ import SchemChecker.SchemComponent as sc
 from openpyxl import Workbook, load_workbook
 
 
+def populate_component():
+
+    COMP_DICT = {
+        '300-23460-0237': sc.SchematicComponent(),
+        '100-46302-2491': sc.SchematicComponent()
+    }
+
+    # 8-pins RELAY
+    COMP_DICT['300-23460-0237'].links.update({
+        'on':[(('5' 'S4'), ('6' 'COM2')),
+              (('5' 'S4'), ('6' 'COM2'))]
+    })
+    COMP_DICT['300-23460-0237'].links.update({
+        'off':[(('5' 'S4'), ('6' 'COM2')),
+               (('5' 'S4'), ('6' 'COM2'))]
+    })
+
+    # 2-pins RESISTOR
+    COMP_DICT['100-46302-2491'].links.update({
+        'passive':[(('5' 'S4'), ('6' 'COM2'))]
+    })
+
+    return COMP_DICT
+
+
 if __name__ == "__main__":
 
     wb = load_workbook('/Users/cahyo/Dropbox/programming/python/SchematicChecker/input_files/P1495_sample.xlsx')
 
     ws = wb.get_active_sheet()
 
-    COMP_DICT = {}
+    SYMBOL_DICT = {}
     NETS_DICT = {}
+    COMP_DICT = populate_component()
     cc = ''
 
     for row in ws.rows:
@@ -18,11 +44,17 @@ if __name__ == "__main__":
                 ch = str(cell.value).replace('\'', '').split(' ')
 
                 if len(ch) == 3:
-                    oo = sc.SchematicComponent()
+                    oo = sc.SchematicSymbol()
                     [_, oo.type, oo.id] = ch
-                    cc = oo.id
 
-                    COMP_DICT.update({oo.id:oo})
+                    if oo.type in COMP_DICT.keys():
+                        oo.links = COMP_DICT[oo.type].links
+                    else:
+                        oo.links = {}
+
+                    SYMBOL_DICT.update({oo.id:oo})
+
+                    cc = oo.id
                     print(ch)
 
             if "Property:" in str(cell.value):
@@ -35,7 +67,7 @@ if __name__ == "__main__":
                 if len(ep) == 5:
                     [_, _, pin_num, pin_name, nets] = ep
 
-                    COMP_DICT[cc].pins.update({(pin_num, pin_name):nets})
+                    SYMBOL_DICT[cc].pins.update({(pin_num, pin_name):nets})
 
 
                     if nets in NETS_DICT.keys():
