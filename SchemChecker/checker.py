9d60e46867ae1d526d91f66f9df6d3563ed65e21
Cahyo <cahyo.p@gmail.com>
2016-03-06 16:35:24 +0800

    figuring out the iteration, manually by using NETS_DICT and SYMBOL_DICT.

--- SchemChecker/checker.py
+++ SchemChecker/checker.py
@@ -11,17 +11,17 @@ def populate_component():
 
     # 8-pins RELAY
     COMP_DICT['300-23460-0237'].links.update({
-        'on':[(('5' 'S4'), ('6' 'COM2')),
-              (('5' 'S4'), ('6' 'COM2'))]
+        'off':[(('3' 'COM1'), ('2' 'S1')),
+              (('6' 'COM2'), ('7' 'S3'))]
     })
     COMP_DICT['300-23460-0237'].links.update({
-        'off':[(('5' 'S4'), ('6' 'COM2')),
-               (('5' 'S4'), ('6' 'COM2'))]
+        'on':[(('3' 'COM1'), ('4' 'S2')),
+               (('6' 'COM2'), ('5' 'S4'))]
     })
 
     # 2-pins RESISTOR
     COMP_DICT['100-46302-2491'].links.update({
-        'passive':[(('5' 'S4'), ('6' 'COM2'))]
+        'passive':[(('1' 'POS'), ('2' 'NEG'))]
     })
 
     return COMP_DICT
@@ -69,7 +69,6 @@ if __name__ == "__main__":
 
                     SYMBOL_DICT[cc].pins.update({(pin_num, pin_name):nets})
 
-
                     if nets in NETS_DICT.keys():
                         NETS_DICT[nets].append((cc, pin_num, pin_name))
                     else:
@@ -78,4 +77,67 @@ if __name__ == "__main__":
 
     pass
 
+'''
+    SYMBOL_DICT['X0'].pins[('90', 'GPIO6')]     #S0_GPIO6
+    NETS_DICT['S0_GPIO6']                       #[('X0', '90', 'GPIO6'), ('R43A', '1', 'POS')]
+    --> [('R43A', '1', 'POS')]
+    --> [('R43A', '2', 'NEG')]
 
+        SYMBOL_DICT['R43A'].pins[('2', 'NEG')]      #J16_HSD_156
+        NETS_DICT['J16_HSD_156']                    #[('J6', 'R12', 'IO68'), ('R43A', '2', 'NEG')]
+        --> [('J6', 'R12', 'IO68')]
+        --> [done]
+        PATH: ['S0_GPIO6', 'J16_HSD_156']
+
+    ==============================================================================================================
+    SYMBOL_DICT['X0'].pins['143', 'GPIO7']      #S0_GPIO7
+    NETS_DICT['S0_GPIO7']                       #[('X0', '143', 'GPIO7'), ('K1A', '3', 'COM1')]
+    --> [('K1A', '3', 'COM1')]
+    --> [('K1A', '2', 'S1'), ('K1A', '4', 'S2')]
+
+        SYMBOL_DICT['K1A'].pins['2', 'S1']          #J16_HSD_157
+        NETS_DICT['J16_HSD_157']                    #[('K1A', '2', 'S1'), ('J6', 'R10', 'IO67')]
+        --> [('J6', 'R10', 'IO67')]
+        --> [done]
+        PATH: ['S0_GPIO7', 'J16_HSD_157']
+
+
+        SYMBOL_DICT['K1A'].pins['4', 'S2']          #$5N2423_110
+        NETS_DICT['$5N2423_110']                    #[('K1A', '4', 'S2'), ('R121A', '2', 'NEG')]
+        --> [('R121A', '2', 'NEG')]
+        --> [('R121A', '1', 'POS')]
+
+            SYMBOL_DICT['R121A'].pins['1', 'POS']       #S0_UVI80_GPIO7
+            NETS_DICT['S0_UVI80_GPIO7']                 #[('K92', '7', 'S3'), ('K92', '2', 'S1'), ('R121A', '1', 'POS')]
+            --> [('K92', '7', 'S3'), ('K92', '2', 'S1')]
+            --> [('K92', '6', 'COM2'), ('K92', '3', 'COM1')]
+
+                SYMBOL_DICT['K92'].pins['6', 'COM2']        #J20_UVI80_4F
+                NETS_DICT['J20_UVI80_4F']                   #[('K92', '6', 'COM2'),
+                                                             ('K92', '3', 'COM1'),
+                                                             ('JP601', '2', 'IO2'),
+                                                             ('J6', 'A15', 'IO8'),
+                                                             ('J6', 'A13', 'IO7')]
+
+                -->[('JP601', '2', 'IO2'), ('J6', 'A15', 'IO8'), ('J6', 'A13', 'IO7')]
+                -->[('JP601', '1', 'IO1'), done, done]
+                PATH: ['S0_GPIO7', '$5N2423_110', 'S0_UVI80_GPIO7', 'J20_UVI80_4F' ]
+
+                    SYMBOL_DICT['JP601'].pins['1', 'IO1']       #J20_UVI80_4S
+                    NETS_DICT['J20_UVI80_4S']                   #[('JP601', '1', 'IO1'), ('J6', 'B14', 'IO17')]
+                    --> [('J6', 'B14', 'IO17')]
+                    --> [done]
+                    PATH: ['S0_GPIO7', '$5N2423_110', 'S0_UVI80_GPIO7', 'J20_UVI80_4F', 'J20_UVI80_4S' ]
+
+                SYMBOL_DICT['K92'].pins['3', 'COM1']        #J20_UVI80_4F
+                NETS_DICT['J20_UVI80_4F']                   #[('K92', '6', 'COM2'),
+                                                             ('K92', '3', 'COM1'),
+                                                             ('JP601', '2', 'IO2'),
+                                                             ('J6', 'A15', 'IO8'),
+                                                             ('J6', 'A13', 'IO7')]
+                --> []
+                --> loop
+
+
+
+'''
