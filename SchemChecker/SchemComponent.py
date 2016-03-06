f7b95f486d1942f127aef3de07cf1be916b201ba
Cahyo <cahyo.p@gmail.com>
2016-03-06 21:36:15 +0800

    use inheritance for the classes.

--- SchemChecker/SchemComponent.py
+++ SchemChecker/SchemComponent.py
@@ -1,16 +1,20 @@
 
-class SchematicSymbol(object):
-
-    def __init__(self):
-        self.id = ''
-        self.type = ''
-        self.pins = {}
-        self.state = []
-        self.links = {}
-
-
 class SchematicComponent(object):
 
     def __init__(self):
         self.type = ''
         self.links = {}
+        self.pins = {}
+
+
+class SchematicSymbol(SchematicComponent):
+
+    def __init__(self):
+        SchematicComponent.__init__()
+        self.id = ''
+        self.state = []
+        # self.type = ''
+        # self.pins = {}
+        # self.links = {}
+
+
