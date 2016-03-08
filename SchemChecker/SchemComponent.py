0d7de21917dcf3623ad329a7c53425a46159b60e
Cahyo <cahyo.p@gmail.com>
2016-03-08 22:48:33 +0800

    adding more component library.

--- SchemChecker/SchemComponent.py
+++ SchemChecker/SchemComponent.py
@@ -1,10 +1,48 @@
 
 class SchematicComponent(object):
 
-    def __init__(self):
+    def __init__(self, standard_type=None):
         self.type = ''
         self.links = {}
         self.pins = {}
+
+        if standard_type == 'std_8pins_relay':
+            self.links.update(
+                {
+                    'off': {('1', 'N1'): [('8', 'N2')],
+                            ('2', 'S1'): [('3', 'COM1')],
+                            ('3', 'COM1'): [('2', 'S1')],
+                            ('4', 'S2'): [],
+                            ('5', 'S4'): [],
+                            ('6', 'COM2'): [('7', 'S3')],
+                            ('7', 'S3'): [('6', 'COM2')],
+                            ('8', 'N2'): [('1', 'N1')]}
+                })
+            self.links.update(
+                {
+                    'on': {('1', 'N1'): [('8', 'N2')],
+                           ('2', 'S1'): [],
+                           ('3', 'COM1'): [('4', 'S2')],
+                           ('4', 'S2'): [('3', 'COM1')],
+                           ('5', 'S4'): [('6', 'COM2')],
+                           ('6', 'COM2'): [('5', 'S4')],
+                           ('7', 'S3'): [],
+                           ('8', 'N2'): [('1', 'N1')]}
+                })
+
+        elif standard_type == 'std_2pins_passive':
+            self.links.update(
+                {
+                    'na': {('1', 'POS'): [('2', 'NEG')],
+                           ('2', 'NEG'): [('1', 'POS')]}
+                })
+
+        elif standard_type == 'std_shorting_bar':
+            self.links.update(
+                {
+                    'na': {('1', 'IO1'): [('2', 'IO2')],
+                           ('2', 'IO2'): [('1', 'IO1')]}
+                })
 
 
 class SchematicSymbol(SchematicComponent):
--- SchemChecker/checker.py
+++ SchemChecker/checker.py
@@ -56,69 +56,30 @@ class PathFinder(object):
     def populate_component(self):
 
         comp_dict = {
-            '300-23460-0237': sc.SchematicComponent(),
-            '100-46302-2491': sc.SchematicComponent(),
-            '100-46312-0000': sc.SchematicComponent(),
-            '100-46302-9093': sc.SchematicComponent(),
-            'EMBEDDED_SHORTING_BAR': sc.SchematicComponent()
+            '300-23460-0237': sc.SchematicComponent('std_8pins_relay'),
+            '100-46302-2491': sc.SchematicComponent('std_2pins_passive'),
+            '100-46312-0000': sc.SchematicComponent('std_2pins_passive'),
+            '100-46302-9093': sc.SchematicComponent('std_2pins_passive'),
+            '100-55258-0105': sc.SchematicComponent('std_2pins_passive'),
+            '105-35077-3222': sc.SchematicComponent('std_2pins_passive'),
+            '100-46302-1002': sc.SchematicComponent('std_2pins_passive'),
+            '105-35273-2475': sc.SchematicComponent('std_2pins_passive'),
+            '100-46312-0103': sc.SchematicComponent('std_2pins_passive'),
+            '100-76445-31R6': sc.SchematicComponent('std_2pins_passive'),
+            '100-46313-0160': sc.SchematicComponent('std_2pins_passive'),
+            '105-35273-2225': sc.SchematicComponent('std_2pins_passive'),
+            '100-46302-1212': sc.SchematicComponent('std_2pins_passive'),
+            '100-46311-0000': sc.SchematicComponent('std_2pins_passive'),
+            'EMBEDDED_SHORTING_BAR': sc.SchematicComponent('std_shorting_bar')
         }
-
-        # 8-pins RELAY
-        comp_dict['300-23460-0237'].links.update({
-            'off': {('1', 'N1'): [('8', 'N2')],
-                    ('2', 'S1'): [('3', 'COM1')],
-                    ('3', 'COM1'): [('2', 'S1')],
-                    ('4', 'S2'): [],
-                    ('5', 'S4'): [],
-                    ('6', 'COM2'): [('7', 'S3')],
-                    ('7', 'S3'): [('6', 'COM2')],
-                    ('8', 'N2'): [('1', 'N1')]}
-            })
-        comp_dict['300-23460-0237'].links.update({
-            'on': {('1', 'N1'): [('8', 'N2')],
-                   ('2', 'S1'): [],
-                   ('3', 'COM1'): [('4', 'S2')],
-                   ('4', 'S2'): [('3', 'COM1')],
-                   ('5', 'S4'): [('6', 'COM2')],
-                   ('6', 'COM2'): [('5', 'S4')],
-                   ('7', 'S3'): [],
-                   ('8', 'N2'): [('1', 'N1')]}
-            })
-
-        # 2-pins RESISTOR
-        comp_dict['100-46302-2491'].links.update({
-            'passive': {('1', 'POS'): [('2', 'NEG')],
-                        ('2', 'NEG'): [('1', 'POS')]}
-        })
-
-        comp_dict['100-46312-0000'].links.update({
-            'passive': {('1', 'POS'): [('2', 'NEG')],
-                        ('2', 'NEG'): [('1', 'POS')]}
-        })
-
-        comp_dict['100-46302-9093'].links.update({
-            'passive': {('1', 'POS'): [('2', 'NEG')],
-                        ('2', 'NEG'): [('1', 'POS')]}
-        })
-
-
-        # jumper
-        comp_dict['EMBEDDED_SHORTING_BAR'].links.update({
-            'passive': {('1', 'IO1'): [('2', 'IO2')],
-                        ('2', 'IO2'): [('1', 'IO1')]}
-        })
 
         return comp_dict
 
-    def find_path(self, symbol, pin_num, pin_name, path=None):
-        if path is None:
-            path = []
-
-        self.seen.append((symbol, pin_num, pin_name))
+    def find_path(self, symbol, pin_num, pin_name, state='init'):
 
         # PROCESS FOR THIS ITERATION
+        self.seen.append((symbol, pin_num, pin_name))
         nets = self.SYMBOL_DICT[symbol].pins[(pin_num, pin_name)]
-        path.append(nets)
 
         if nets == 'unconnected':
             ports_at_nets = []
@@ -126,10 +87,10 @@ class PathFinder(object):
             ports_at_nets = [x for x in self.NETS_DICT[nets] if x != (symbol, pin_num, pin_name)]
 
         # PRINT OUT FOR DEBUG
-        starting_symbol_string = ' ' + '|'.join([symbol, pin_num, pin_name])
-        final_symbol_string = ' & '.join(['%s|%s|%s' % (x,y,z) for x,y,z in ports_at_nets])
+        starting_symbol_string = ' ' + '|'.join([symbol, state, pin_num, pin_name])
+        final_symbol_string = ' & '.join(['|'.join([x, y, z]) for x, y, z in ports_at_nets])
         sep = ' --> '
-        print (self.tab + starting_symbol_string + sep + nets + sep + final_symbol_string)
+        print(self.tab + starting_symbol_string + sep + nets + sep + final_symbol_string)
 
         # PROCESS FOR THE NEXT ITERATION
         filtered_ports = self.remove_previous_ports(ports_at_nets, self.seen)
@@ -137,18 +98,21 @@ class PathFinder(object):
         self.seen.extend([(x, y, z) for (x, _, y, z) in processed_ports])
 
         if processed_ports:
-            self.tab = self.tab + '--'
-            for (x, _, y, z) in processed_ports:
-                self.find_path(x, y, z, path=path)
+            self.tab += '--'
+            for (x, s, y, z) in processed_ports:
+                self.find_path(x, y, z, s)
             self.tab = self.tab[:-2]
         else:
             pass
 
         # return path
 
-    def remove_previous_ports(self, ports, seen=None):
+    @staticmethod
+    def remove_previous_ports(ports, seen=None):
         if seen is None:
             seen = []
+
+        # print (str([x for x in ports if x in seen]))
         return [x for x in ports if x not in seen]
 
     def process_ports(self, ports):
@@ -174,27 +138,27 @@ if __name__ == "__main__":
     xlsx_file = '/Users/cahyo/Dropbox/programming/python/SchematicChecker/input_files/P1495_sample.xlsx'
     pf = PathFinder()
     pf.populate_dictionaries(xlsx_file)
-    print ('=' * 80)
-    pf.clear_found_ports()
-    pf.find_path('X0', '90', 'GPIO6')
-
-    print ('=' * 80)
+    # print('=' * 80)
+    # pf.clear_found_ports()
+    # pf.find_path('X0', '90', 'GPIO6')
+    #
+    print('=' * 80)
     pf.clear_found_ports()
     pf.find_path('X0', '143', 'GPIO7')
+    #
+    # print('=' * 80)
+    # pf.clear_found_ports()
+    # pf.find_path('X0', '110', 'MPP3')
+    #
+    # print('=' * 80)
+    # pf.clear_found_ports()
+    # pf.find_path('X2', '110', 'MPP3')
+    #
+    # print('=' * 80)
+    # pf.clear_found_ports()
+    # pf.find_path('J6', 'T8', 'IO85')
 
-    print ('=' * 80)
-    pf.clear_found_ports()
-    pf.find_path('X0', '110', 'MPP3')
-
-    print ('=' * 80)
-    pf.clear_found_ports()
-    pf.find_path('X2', '110', 'MPP3')
-
-    print ('=' * 80)
-    pf.clear_found_ports()
-    pf.find_path('J6', 'T8', 'IO85')
-
-    print ('=' * 80)
+    print('=' * 80)
     pf.clear_found_ports()
     pf.find_path('J6', 'A15', 'IO8')
     pass
